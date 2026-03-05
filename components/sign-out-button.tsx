f1588f75afb119b5e9c8864903d0aeb79e7cbb7a
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-03-05 14:23:22 -0500

    Login, Profile, Sign Out

---
+++ components/sign-out-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { signOut } from "@/lib/auth-client"
+import { toast } from "sonner"
+
+export const SignOutButton = () => {
+    const router = useRouter()
+    async function handleClick() {
+        await signOut({
+            fetchOptions: {
+                onError: (ctx) => {
+                    toast.error(ctx.error.message);
+                },
+                onSuccess: () => {
+                    router.push("/auth/login")
+                }
+            }
+        })
+    }
+
+    return (
+        <Button onClick={handleClick} size="sm" variant="destructive" className = "bg-black">
+            Sign Out
+        </Button>
+    );
+}
